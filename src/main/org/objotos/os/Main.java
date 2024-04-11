8f6228e5b1af53e5b483806d2758859cf20227bf
ferderplays <86313430+ferderplays@users.noreply.github.com>
2024-04-11 21:48:04 +0200

    some minor shit

---
+++ src/main/org/objotos/os/Main.java
@@ -0,0 +1,7 @@
+package org.objotos.os;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/org/objotos/ui/Window.java
@@ -0,0 +1,7 @@
+package org.objotos.ui;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+    
+}
